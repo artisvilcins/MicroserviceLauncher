e27480c1cc2f639756b3e13bd0a6b31f7914a033
artis.vilcins <artis.vilcins@visma.com>
2022-11-27 14:14:33 +0200

    Update to .NET7

--- MicroserviceLauncher.Ui/App.xaml.cs
+++ MicroserviceLauncher.Ui/App.xaml.cs
@@ -1,17 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Configuration;
-using System.Data;
-using System.Linq;
-using System.Threading.Tasks;
-using System.Windows;
+﻿namespace MicroserviceLauncher.Ui;
 
-namespace MicroserviceLauncher.Uiw
+/// <summary>
+/// Interaction logic for App.xaml
+/// </summary>
+public partial class App
 {
-    /// <summary>
-    /// Interaction logic for App.xaml
-    /// </summary>
-    public partial class App : Application
-    {
-    }
 }
--- MicroserviceLauncher.Ui/MainWindow.xaml.cs
+++ MicroserviceLauncher.Ui/MainWindow.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Diagnostics;
+using System.Linq;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -8,145 +9,140 @@ using System.Windows.Media;
 using MicroserviceLauncher.Ui.Models;
 using MicroserviceLauncher.Ui.Services;
 
-namespace MicroserviceLauncher.Ui
+namespace MicroserviceLauncher.Ui;
+
+public partial class MainWindow
 {
-    public partial class MainWindow : Window
+    private readonly ObservableCollection<MicroserviceRow> _microserviceConfigs;
+
+    private IEnumerable<MicroserviceRow> MicroserviceConfigs => _microserviceConfigs;
+
+    private readonly MicroserviceActionsService _microserviceActions;
+
+
+    private readonly Dictionary<MicroserviceRow, Process> _processes;
+
+    public MainWindow()
     {
-        private readonly ObservableCollection<MicroserviceRow> _microserviceConfigs;
+        InitializeComponent();
 
-        private IEnumerable<MicroserviceRow> MicroserviceConfigs => _microserviceConfigs;
+        _processes = new Dictionary<MicroserviceRow, Process>();
+        _microserviceActions = new MicroserviceActionsService();
 
-        private readonly MicroserviceActionsService _microserviceActions;
+        _microserviceConfigs = new ObservableCollection<MicroserviceRow>();
 
+        var repo = new MicroserviceConfigRepository();
 
-        private readonly Dictionary<MicroserviceRow, Process> _processes;
-
-        public MainWindow()
+        foreach (var service in repo.GetMicroservices())
         {
-            InitializeComponent();
-
-            _processes = new Dictionary<MicroserviceRow, Process>();
-            _microserviceActions = new MicroserviceActionsService();
-
-            _microserviceConfigs = new ObservableCollection<MicroserviceRow>();
-
-            var repo = new MicroserviceConfigRepository();
-
-            foreach (var service in repo.GetMicroservices())
+            var conf = new MicroserviceRow
             {
-                var conf = new MicroserviceRow
-                {
-                    Name = service.Name,
-                    LaunchPath = service.LaunchPath,
-                    GitPath = service.GitPath,
-                    IsRunning = false,
-                    ProcessName = service.ProcessName,
-                    IsMandatory = service.IsMandatory
-                };
+                Name = service.Name,
+                LaunchPath = service.LaunchPath,
+                GitPath = service.GitPath,
+                IsRunning = false,
+                ProcessName = service.ProcessName,
+                IsMandatory = service.IsMandatory
+            };
 
-                _microserviceConfigs.Add(conf);
+            _microserviceConfigs.Add(conf);
+        }
+
+        foreach (var p in Process.GetProcesses())
+        {
+            foreach (var configItem in _microserviceConfigs)
+            {
+                if (!string.IsNullOrWhiteSpace(configItem.ProcessName) && p.ProcessName == configItem.ProcessName)
+                {
+                    p.EnableRaisingEvents = true;
+
+                    _processes.Add(configItem, p);
+                    p.Exited += (_, _) => { configItem.IsRunning = false; };
+                    configItem.IsRunning = true;
+                }
             }
+        }
 
-            foreach (var p in Process.GetProcesses())
+
+        foreach (var microserviceConfig in _microserviceConfigs)
+        {
+            microserviceConfig.IsRunningChange += change =>
             {
-                foreach (var configItem in _microserviceConfigs)
+                if (change)
                 {
-                    if (!string.IsNullOrWhiteSpace(configItem.ProcessName) && p.ProcessName == configItem.ProcessName)
+                    StartMicroservice(microserviceConfig);
+                }
+                else
+                {
+                    if (!_processes.TryGetValue(microserviceConfig, out var process))
                     {
-                        p.EnableRaisingEvents = true;
-
-                        _processes.Add(configItem, p);
-                        p.Exited += (s, e) =>
-                        {
-                            configItem.IsRunning = false;
-                        };
-                        configItem.IsRunning = true;
+                        return;
                     }
+
+                    process.Kill();
+                    _processes.Remove(microserviceConfig);
                 }
-            }
-
-
-            foreach (var microserviceConfig in _microserviceConfigs)
-            {
-                microserviceConfig.IsRunningChange += change =>
-                {
-                    if (change)
-                    {
-                        var newProcess = _microserviceActions.StartMicroservice(microserviceConfig);
-                        newProcess.EnableRaisingEvents = true;
-                        
-                        _processes.Add(microserviceConfig, newProcess);
-
-                        newProcess.Exited += (s, e) =>
-                        {
-                            microserviceConfig.IsRunning = false;
-                        };
-                    }
-                    else
-                    {
-                        if (_processes.TryGetValue(microserviceConfig, out Process process))
-                        {
-                            process.Kill();
-
-                            _processes.Remove(microserviceConfig);
-                        }
-                    }
-                };
-            }
-
-            MicroservicesGrid.ItemsSource = _microserviceConfigs;
+            };
         }
 
-        private void StartMandatoryServices_Click(object sender, RoutedEventArgs e)
+        MicroservicesGrid.ItemsSource = _microserviceConfigs;
+    }
+
+    private void StartMicroservice(MicroserviceRow microserviceConfig)
+    {
+        var newProcess = _microserviceActions.StartMicroservice(microserviceConfig);
+        newProcess.EnableRaisingEvents = true;
+
+        _processes.Add(microserviceConfig, newProcess);
+
+        newProcess.Exited += (_, _) => { microserviceConfig.IsRunning = false; };
+    }
+
+    private void StartMandatoryServices_Click(object sender, RoutedEventArgs e)
+    {
+        foreach (var microservice in _microserviceConfigs)
         {
-            foreach (var microservice in _microserviceConfigs)
+            if (!microservice.IsMandatory || microservice.IsRunning)
             {
-                if (microservice.IsMandatory && !microservice.IsRunning)
-                {
-                    microservice.IsRunning = true;
-                    // StartMicroservice(microservice);
-                }
-            }
-            
-            MicroservicesGrid.Items.Refresh();
-        }
-        
-        private void PullFromGit_Click(object sender, RoutedEventArgs e)
-        {
-            for (var vis = sender as Visual; vis != null; vis = VisualTreeHelper.GetParent(vis) as Visual)
-            {
-                if (vis is not DataGridRow row)
-                {
-                    continue;
-                }
-                
-                var item = (MicroserviceRow)row.Item;
-                _microserviceActions.PullFromGit(item);
-                break;
-            }
-        }
-
-        private async void PullAllServices_Click(object sender, RoutedEventArgs e)
-        {
-            Parallel.ForEach(MicroserviceConfigs, new ParallelOptions { MaxDegreeOfParallelism = 4 },
-                service =>
-                {
-                    _microserviceActions.PullFromGit(service);
-                });
-        }
-
-        private void StopAllServices_Click(object sender, RoutedEventArgs e)
-        {
-            foreach (var process in _processes)
-            {
-                if (process.Value != null)
-                {
-                    process.Value.Kill();
-                    process.Key.IsRunning = false;
-                }
+                continue;
             }
 
-            MicroservicesGrid.Items.Refresh();
+            microservice.IsRunning = true;
+            _microserviceActions.StartMicroservice(microservice);
         }
+
+        MicroservicesGrid.Items.Refresh();
+    }
+
+    private void PullFromGit_Click(object sender, RoutedEventArgs e)
+    {
+        for (var vis = sender as Visual; vis != null; vis = VisualTreeHelper.GetParent(vis) as Visual)
+        {
+            if (vis is not DataGridRow row)
+            {
+                continue;
+            }
+
+            var item = (MicroserviceRow)row.Item;
+            _microserviceActions.PullFromGit(item);
+            break;
+        }
+    }
+
+    private void PullAllServices_Click(object sender, RoutedEventArgs e)
+    {
+        Parallel.ForEach(MicroserviceConfigs, new ParallelOptions { MaxDegreeOfParallelism = 4 },
+            service => { _microserviceActions.PullFromGit(service); });
+    }
+
+    private void StopAllServices_Click(object sender, RoutedEventArgs e)
+    {
+        foreach (var process in _processes.Where(process => process.Value != null))
+        {
+            process.Value.Kill();
+            process.Key.IsRunning = false;
+        }
+
+        MicroservicesGrid.Items.Refresh();
     }
 }
